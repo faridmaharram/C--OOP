247b7f0b4511aed99abbd36efc979c506ec5b501
faridmaharram <faridmaharramov@hotmail.com>
2019-03-07 15:48:22 +0300

    Interfaces Added

---
+++ CSharp-OOP/Interfaces/ICustomerDAL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDAL customerDAL)
+        {
+            customerDAL.Add();
+        }
+    }
+
+    interface ICustomerDAL
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDAL : ICustomerDAL
+    {
+        public void Add()
+        {
+            Console.WriteLine("SQL Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("SQL Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("SQL Updated");
+        }
+    }
+
+    class OracleCustomerDAL : ICustomerDAL
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+           Console.WriteLine("Oracle Updated");
+        }
+    }
+}
--- CSharp-OOP/Interfaces/Program.cs
+++ CSharp-OOP/Interfaces/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Interfaces
 {
+    // İnterface`ler  genel olarak servis implementasyonlarında ve katmanlar arası geçişler zamanı kullanılır.
     class Program
     {
         static void Main(string[] args)
         {
+            // Example1();
+            //  Example2();
+
+            Console.ReadLine();
+        }
+
+        private static void Example2()
+        {
+            CustomerManager customer = new CustomerManager();
+            customer.Add(new SqlServerCustomerDAL());
+            customer.Add(new OracleCustomerDAL());
+        }
+
+        private static void Example1()
+        {
+            CustomManager customManager = new CustomManager();
+            customManager.Add(new Customer { Id = 1, Name = "Farid", Surname = "Maharramov" });
+            customManager.Add(new Student { Id = 1, Name = "Nur", Surname = "İsmayılova" });
         }
     }
     // İnterface`ler soyut nesnelerdir, tek başına bhiç bir anlam ifade etmez
@@ -22,13 +41,29 @@ namespace Interfaces
         string Surname { get; set; }
     }
     // Class`lar ise somut nesnelerdir
-    class Customer
+    class Customer : IPerson
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
     }
 
-    class Student
+    class Student : IPerson
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+
+    class CustomManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine("Customer " + person.Name + " Added");
+        }
     }
 }
