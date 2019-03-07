75cdeb0b5093bd00d21271d2fab30e409a424c89
faridmaharram <faridmaharramov@hotmail.com>
2019-03-07 14:43:23 +0300

    Classes Added

---
+++ CSharp-OOP/Classes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        //Bir sınıf,içerisinde o sınıfile ilgili metotları barındırır
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+            customerManager.Delete();
+
+
+            // 1. şelikde değer atama
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Farid";
+            customer.Surname = "Maharramov";
+
+
+            // 2. şekilde değer atama
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                Name = "Hummat",
+                Surname = "Maharramov"
+            };
+
+            Console.WriteLine(customer.Name+" ve "+customer2.Name);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Updated");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Deleted");
+        }
+    }
+}
---
+++ CSharp-OOP/Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    // İnterface`ler soyut nesnelerdir, tek başına bhiç bir anlam ifade etmez
+    interface IPerson
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        string Surname { get; set; }
+    }
+    // Class`lar ise somut nesnelerdir
+    class Customer
+    {
+
+    }
+
+    class Student
+    {
+
+    }
+}
