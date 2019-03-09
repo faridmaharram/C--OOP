be1a9d38848a603cb8ec5392bf1b5bfc1d040e4a
faridmaharram <faridmaharramov@hotmail.com>
2019-03-09 10:45:15 +0300

    Virtual Methods Added

---
+++ CSharp-OOP/VirtualMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServerDatabase sqlServerDatabase = new SqlServerDatabase();
+            sqlServerDatabase.Add();
+            MysqlDatabase mysqlDatabase = new MysqlDatabase();
+            mysqlDatabase.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by defaul");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+
+    class SqlServerDatabase : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql");
+          //  base.Add();
+        }
+    }
+    class MysqlDatabase : Database
+    {
+
+    }
+}
