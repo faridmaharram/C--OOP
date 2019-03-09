817077fac1a26041ed341016cea0eb7ea18b45e7
faridmaharram <faridmaharramov@hotmail.com>
2019-03-09 09:57:08 +0300

    Inheritance

---
+++ CSharp-OOP/Inheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        //Bir sınıf birden fazla sınıfı mireas alamaz, ama birden fazla interface alabilir
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Person{ Name="Farid" },
+                new Customer{ Name="Maksat"},
+                new Student{Name="Nur"}
+            };
+
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+
+    class Customer : Person
+    {
+
+    }
+
+    class Student : Person
+    {
+
+    }
+}
