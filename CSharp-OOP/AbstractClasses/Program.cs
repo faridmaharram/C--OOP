0f426cd136113257d2f3b63cee9a8d3446cb68da
faridmaharram <faridmaharramov@hotmail.com>
2019-03-10 13:15:29 +0300

    Abstract Classes Added

---
+++ CSharp-OOP/AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new SqlDb();
+            database.Add();
+            database.Delete();
+
+            Database database2 = new OracleDb();
+            database2.Add();
+            database2.Delete();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+        public abstract void Delete();
+    }
+
+    class SqlDb : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+
+    class OracleDb : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
